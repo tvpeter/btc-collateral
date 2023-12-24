ad220454173e05af617affc0f56485302d8937cc
Vihiga Tyonum <withtvpeter@gmail.com>
2023-12-24 21:58:33 +0100

    improve error message

--- src/domain/generate_address.rs
+++ src/domain/generate_address.rs
@@ -93,14 +93,14 @@ use super::*;
    #[test]
    fn test_redeem_script_hex(){
     let pubkey_string = "02f0eaa04e609b0044ef1fe09a350dc4b744a5a8604a6fa77bc9bf6443ea50739f";
-    let borrower_pubkey = PublicKey::from_str(pubkey_string).expect("pubkey");
+    let borrower_pubkey = PublicKey::from_str(pubkey_string).expect("Invalid borrower pubkey");
 
     let lender_pubkey = PublicKey::from_str(
         "037c60db011a840523f216e7198054ef071c5acd3d4b466cf2658b7faf30c11e33",
     )
-    .expect("pubkey");
+    .expect("Invalid lender pubkey");
 
-    let service_pubkey = PublicKey::from_str("02ca49f36d3de1e135e033052611dd0873af55b57f07d5d0d1090ceb267ac34e6b").expect("pubkey");
+    let service_pubkey = PublicKey::from_str("02ca49f36d3de1e135e033052611dd0873af55b57f07d5d0d1090ceb267ac34e6b").expect("Invalid service pubkey");
 
     let combined_keys = PartiesPublicKeys::new(borrower_pubkey, lender_pubkey, service_pubkey);
 
--- src/main.rs
+++ src/main.rs
@@ -8,15 +8,15 @@ use domain::generate_address::PartiesPublicKeys;
 
 fn main() {
     let pubkey_string = "02f0eaa04e609b0044ef1fe09a350dc4b744a5a8604a6fa77bc9bf6443ea50739f";
-    let borrower_pubkey = PublicKey::from_str(pubkey_string).expect("pubkey");
+    let borrower_pubkey = PublicKey::from_str(pubkey_string).expect("invalid borrower pubkey");
 
     let lender_pubkey = PublicKey::from_str(
         "037c60db011a840523f216e7198054ef071c5acd3d4b466cf2658b7faf30c11e33",
     )
-    .expect("pubkey");
+    .expect("invalid lender pubkey");
 
 
-    let service_pubkey = PublicKey::from_str("02ca49f36d3de1e135e033052611dd0873af55b57f07d5d0d1090ceb267ac34e6b").expect("pubkey");
+    let service_pubkey = PublicKey::from_str("02ca49f36d3de1e135e033052611dd0873af55b57f07d5d0d1090ceb267ac34e6b").expect("invalid service pubkey");
 
     let combined_keys = PartiesPublicKeys::new(borrower_pubkey, lender_pubkey, service_pubkey);
 
