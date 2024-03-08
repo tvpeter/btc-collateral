423a5d5ec80fd11e2f9c1bdbd57f4e62e594515d
Jeezman <tobiadeyemi91@gmail.com>
2024-03-08 15:21:07 +0100

    Database setup - add db config - add db connection to app state
    Signed-off-by: Jeezman <tobiadeyemi91@gmail.com>

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,14 @@
 use btc_collateral::{config::Settings, startup::run};
+use sqlx::{Connection, PgConnection};
 use std::net::TcpListener;
 
 #[tokio::main]
 async fn main() -> std::io::Result<()> {
 	let settings = Settings::get_configuration().expect("failed to read config");
+	let connection = PgConnection::connect(&settings.database.connection_string())
+		.await
+		.expect("Failed to connect to postgres");
 	let address = format!("127.0.0.1:{}", settings.application_port);
 	let listener = TcpListener::bind(address).expect("Failed to bind random port");
-	run(listener)?.await
+	run(listener, connection)?.await
 }
