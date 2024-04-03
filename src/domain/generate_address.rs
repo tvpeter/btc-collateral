04d85b15e95010a76e3d8a3eb1c604a49b24b62e
Vihiga Tyonum <withtvpeter@gmail.com>
2024-04-03 16:58:55 +0100

    Refactor: Remove validate pubkeys method
    Since the PartiesPublicKeys struct only accepted
    valid public keys, it is assumed that only
    valid keys will be supplied. As such, the
    method was not used anywhere in the codebase.
    Also, the validation method is available
    in the Rust Bitcoin library.
    
    [Ticket: X]

--- src/domain/generate_address.rs
+++ src/domain/generate_address.rs
@@ -23,25 +23,8 @@ impl PartiesPublicKeys {
 		}
 	}
 
-	fn validate_pubkeys(&self) -> Result<Self, String> {
-		let borrower_pubkey = PublicKey::from_slice(&self.borrower_pubkey.to_bytes())
-			.map_err(|e| format!("Invalid borrower pubkey: {:?}", e))?;
-		let lender_pubkey = PublicKey::from_slice(&self.lender_pubkey.to_bytes())
-			.map_err(|e| format!("Invalid lender pubkey: {:?}", e))?;
-		let service_pubkey = PublicKey::from_slice(&self.service_pubkey.to_bytes())
-			.map_err(|e| format!("Invalid service pubkey: {:?}", e))?;
-
-		Ok(Self {
-			borrower_pubkey,
-			lender_pubkey,
-			service_pubkey,
-		})
-	}
-
 	///OP_2  [pubkey1] [pubkey2] [pubkey3] OP_3 OP_CHECKMULTISIG
 	pub fn redeem_script(&self) -> ScriptBuf {
-		let _ = self.validate_pubkeys();
-
 		Builder::new()
 			.push_opcode(OP_PUSHNUM_2)
 			.push_key(&self.borrower_pubkey)
@@ -92,12 +75,6 @@ mod tests {
 		let combined_keys = valid_publickeys();
 
 		assert_eq!(combined_keys.redeem_script().to_hex_string(), "522102f0eaa04e609b0044ef1fe09a350dc4b744a5a8604a6fa77bc9bf6443ea50739f21037c60db011a840523f216e7198054ef071c5acd3d4b466cf2658b7faf30c11e332102ca49f36d3de1e135e033052611dd0873af55b57f07d5d0d1090ceb267ac34e6b53ae");
-	}
-
-	#[test]
-	fn test_validate_publickeys() {
-		let valid_instance = valid_publickeys();
-		assert!(std::panic::catch_unwind(|| valid_instance.validate_pubkeys()).is_ok());
 	}
 
 	#[test]
